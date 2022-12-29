fe5af0eb2550587eb3526030e254141f70e7906b
hosung <winsomed96@naver.com>
2022-12-29 14:17:17 +0900

    "fix : update file's path"

---
+++ src/main/java/ArrayList/ArrayList_Test.java
@@ -0,0 +1,34 @@
+package ArrayList;
+
+
+
+public class ArrayList_Test {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> original = new ArrayList<Integer>();
+        original.add(10);
+
+
+
+        ArrayList<Integer> copy =original;
+
+        copy.add(20);
+        System.out.println( original.contains(10));
+        original.remove((Integer)10);
+        System.out.println(original.isEmpty());
+        System.out.println( original.contains(10));
+        System.out.println("original list");
+        for(int i = 0; i < original.size(); i++) {
+            System.out.println("index " + i + " data = " + original.get(i));
+        }
+
+        System.out.println("\ncopy list");
+        for(int i = 0; i < copy.size(); i++) {
+            System.out.println("index " + i + " data = " + copy.get(i));
+        }
+
+    }
+
+}
